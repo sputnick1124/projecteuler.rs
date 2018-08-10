cef8d12f9cd532aa3be0a9ce7b3b0c4df3ac0523
Nicklas O. Stockton <stocktonno@b1452ua144054r.rz.afrl-wrs.hpc.mil>
2018-08-10 09:55:27 -0400

    add problem descriptions for problems 17, 18, and 20

--- src/lib.rs
+++ src/lib.rs
@@ -667,6 +667,41 @@ pub fn euler016(power: usize) -> BigUint {
     add_digits(pow(two, power))
 }
 
+/// If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are
+/// `3+3+5+4+4=19` letters used in total.
+///
+/// If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many
+/// letters would be used?
+///
+/// __Note__: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two)
+/// contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and"
+/// when writing out numbers is in compliance with British usage.
+pub fn euler017() {
+}
+
+/// You are given the following information, but you may prefer to do some research for yourself.
+///
+///  - 1 Jan 1900 was a Monday
+///  - Thirty days has September,  
+///  April, June and November.  
+///  All the rest have thirty-one,  
+///  Saving February alone,  
+///  Which has twenty-eight, rain or shine.  
+///  And on leap years, twenty-nine.
+///  - A leap year occurs on any year evenly divisible by 4, but not on a century unless it is
+///  divisible by 400.
+///
+/// How many Sundays fell on the first day of the month during the twentieth century (1 Jan 1900 to
+/// 31 Dec 2000)?
+pub fn euler019() {
+}
+
+/// `n!` means `n*(n-1)*...*3**2*1`
+///
+/// For example, `10! = 10*9*...*3*2*1 = 3628800`, and the sum of the digits in the number `10!` is
+/// `3+6+2+8+8+0+0 = 27`.
+///
+/// Find the sum of the digits in the number `100!`
 pub fn euler020(n: BigUint) -> BigUint {
     add_digits(fact(n))
 }

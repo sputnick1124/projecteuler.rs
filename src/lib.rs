ed5f1e855f8fd099952e7671016c13459c9d2002
Nick Stockton <sputnick1124@comcast.net>
2018-08-12 19:35:03 -0400

    add docs for problem 21

--- src/lib.rs
+++ src/lib.rs
@@ -797,6 +797,20 @@ pub fn euler019() {
 pub fn euler020(n: BigUint) -> BigUint {
     add_digits(fact(n))
 }
+
+/// Let *d(n)* be defined as the sum of proper divisors of *n* (numbers less than *n* which divide
+/// evenly into *n*).  
+/// If *d(a) = b* and *d(b) = a*, where *a != b*, then *a* and *b* are an amicable pair and each of
+/// *a* and *b* are called amicable numbers.
+///
+/// For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55, and 110;
+/// therefore *d(220) = 284*. The proper divisors of 284 are 1, 2, 4, 71, and 142; so *d(284) =
+/// 220*.
+///
+/// Evaluate the sum of all the amicable numbers under 1000.
+pub fn euler021() -> u64 {
+}
+
 pub fn euler067() -> Vec<u64> {
     euler018("euler067.txt".to_string())
 }

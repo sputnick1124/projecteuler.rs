06060ae5532c27e292505ed52594885ec8db100c
Nick Stockton <sputnick1124@comcast.net>
2018-08-19 23:42:22 -0400

    add docs for problems 22, 23

--- src/lib.rs
+++ src/lib.rs
@@ -880,6 +880,38 @@ fn test_euler021() {
     assert_eq!(31626, euler021(10_000));
 }
 
+/// Using a list of over five-thousand first names, begin by sorting it into alphabetical order.
+/// Then working out the alphabetical value for eah name, multiply this calue by its lphabetical
+/// position in te list to obtain a name score.
+///
+/// For example, when the list is sorted into alphabetical order, COLIN, which is wirth
+/// `3+15+12+9+14=53`, is the 938th name in the list. So, COLIN would abtain a score of
+/// `936*53=49714`.
+///
+/// What is the total of all the name scores in the list?
+pub fn euler022() -> u64 {
+    unimplemented!();
+}
+
+/// A perfect number is a number for which the sum of its proper divisors is exactly equal to the
+/// number. For example, the sum of the proper divisors of 28 would be `1+2+4+7+14=28`, which means
+/// that `28` is a perfect number.
+///
+/// A number *n* is called deficient if the sum of its proper divisors is less than *n* and it is
+/// called abundant if this sum is exceeds *n*.
+///
+/// As 12 is the smallest abundant number, `1+2+3+4+6=16`, the smallest number that can be written
+/// as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all
+/// integers greater than 28123 can be written as the sum of two abundant numbers. However, this
+/// upper limit cannot be reduced any further by analysis even though it is known that the greatest
+/// number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+///
+/// Find the sum of all positive integers which cannot be expressed as the sum of two abundant
+/// numbers.
+pub fn euler023() -> u64 {
+    unimplemented!();
+}
+
 /// The Fibonacci sequence is defined by the recurrence relation:  
 /// *F_n = F_{n-1} + F_{n-2}*, where *F_1=1* and *F_2=1*.
 ///

cf732485755aa1d6b32b2094e50bb7d886643e0b
Nicklas Stockton <nicklas.stockton@us.af.mil>
2018-08-20 22:47:09 -0400

    start cli with clap.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 extern crate euler;
 extern crate clap;
 
+use clap::{Arg, App};
 use euler::*;
 
 fn main() {
+    let matches = App::new("Euler")
+                        .version("0.1.0")
+                        .author("Nick Stockton <sputnick1124@comcast.net>")
+                        .about("Solutions for projecteuler.net problems")
+                        .arg(Arg::with_name("problem")
+                             .short("p")
+                             .long("problem")
+                             .value_name("NUMBER")
+                             .help("Selects which problem to solve")
+                             .takes_value(true)
+                             //.required(true)
+                             //.index(1))
+                        )
+                        .get_matches();
+
+    
+
 
     println!("{}", euler008(4));
 }
